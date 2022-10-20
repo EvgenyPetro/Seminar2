fc50983b290df4c737b308c9c945350c954fcb66
Evgeny Petrov <petroves88@mail.ru>
2022-10-20 16:15:15 +0300

    Solution2

---
+++ Solution2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трехзначное число: ");
+String numberA = Console.ReadLine();
+
+
+char[] characters = numberA.ToCharArray();
+
+try
+{
+    Console.WriteLine(characters[2]);
+}
+catch (IndexOutOfRangeException)
+{
+    Console.Write("третьей цифры нет");
+}
