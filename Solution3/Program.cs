774abd6e2841121fe3ef36e822333f69700a8aef
Evgeny Petrov <petroves88@mail.ru>
2022-10-20 16:25:02 +0300

    Solution3

---
+++ Solution3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+if (5 < numberA && numberA < 8)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
+
+
