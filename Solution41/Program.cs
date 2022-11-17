628ba82596b45493b51f0a956bf55a01579c5163
Evgeny Petrov <petroves88@mail.ru>
2022-11-17 17:19:08 +0300

    add solution 41,43

---
+++ Solution41/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//    0, 7, 8, -2, -2 -> 2
+//    1, -7, 567, 89, 223-> 4
+
+Console.WriteLine("Введите числа через запятую:");
+string? numbers = Console.ReadLine();
+
+
+int GetPositiveCountOfNumbers(string stringNumbers)
+{
+    string[] stringArray = stringNumbers.Split(",", StringSplitOptions.RemoveEmptyEntries);
+    int positivCount = 0;
+
+    try
+    {
+        foreach (var t in stringArray)
+        {
+            int number = Convert.ToInt32(t);
+            if (number > 0)
+            {
+                positivCount++;
+            }
+        }
+    }
+    catch
+    {
+        throw new Exception("Должны быть введены только целые числа, (ошибка преобразования):");
+    }
+
+    return positivCount;
+}
+
+try
+{
+    if (numbers != null) Console.WriteLine($"Чисел больше нуля: {GetPositiveCountOfNumbers(numbers)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+
