079c9976abb8aaba07ab724439b88a139aeb6817
Evgeny Petrov <petroves88@mail.ru>
2022-11-26 23:07:50 +0300

    Solution 64, 66, 68

---
+++ Solution64/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Задайте значение N");
+int natureNumber = Convert.ToInt32(Console.ReadLine());
+
+void WriteNumber(int natureNumber)
+{
+    if (natureNumber < 1)
+    {
+        return;
+    }
+
+    Console.Write($"{natureNumber} ");
+    WriteNumber(--natureNumber);
+}
+
+WriteNumber(natureNumber);
---
+++ Solution66/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Задайте значение M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте значение N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumNaturalElement(int M, int N)
+{
+    if (M > N)
+    {
+        throw new Exception("Число M должно быть меньше числа N");
+    }
+    else if (M == N)
+    {
+        return 0;
+    }
+    return SumBetweenElement(M, N);
+}
+
+int SumBetweenElement(int natureNumber, int N)
+{
+    if (M >= N)
+    {
+        return N;
+    }
+    return N + SumBetweenElement(M, --N);
+}
+
+
+try
+{
+    Console.WriteLine($"Sum = {SumNaturalElement(M, N)}");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
